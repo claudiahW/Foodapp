c6a8fa014e39f4f0717623e3a581f6fa58279083
claudiahW <murungiclaud@gmail.com>
2024-02-07 04:04:42 +0300

    make API calls

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
 
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "69b76754940b4f86ba578a8ae490bc9f";
+
 export default function Search() {
     const [query, setQuery] = useState("pizza");
 
     useEffect(() => {
-      function demo() {
-      console.log("Demo function executed")  
-      }  
-      demo();
+     async function fetchFood(){
+     const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+     const data = await res.json();
+     console.log(data.results);
+     }
+     fetchFood()
     }, [query]);
      return (
         <div>
