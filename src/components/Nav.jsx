72249f380d1d11aad3f879e366e2f6289b21d49e
claudiahW <murungiclaud@gmail.com>
2024-02-07 05:42:15 +0300

    style the nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
+import styles from "./nav.module.css";
 export default function Nav() {
     return (
-        <div> 🍔 FoodApp </div>
+        <div className={styles.nav}> 🍔 FoodApp </div>
     )
 };
