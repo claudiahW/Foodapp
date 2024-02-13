79e5ebb6ed63669d5c1b7d60b0eb49085db661aa
claudiahW <murungiclaud@gmail.com>
2024-02-13 13:04:24 +0300

    style the search bar

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import styles from "./fooddetails.module.css";
+import ItemList from "./ItemList";
 export default function FoodDetails({ foodId }) {
     const [food, setFood] = useState({});
     const [isLoading, setIsLoading] = useState(true);
@@ -20,6 +21,7 @@ export default function FoodDetails({ foodId }) {
         <div className={styles.recipeCard}>
             <h1 className={styles.recipeName}>{food.title}</h1>
             <img className={styles.recipeImage} src={food.image} alt="" />
+            
        <div className={styles.recipedetails}> 
 
     <span>
@@ -40,11 +42,14 @@ export default function FoodDetails({ foodId }) {
       💵 <span><strong>{food.pricePerServing/100} Per Serving</strong></span>
     </div>
       </div>
-       
+      
+        <h2>Ingredients</h2>
+        <ItemList food={food} isLoading={isLoading} />
         <h2>Instructions</h2>
+
         <div className={styles.recipeInstructions}>
          <ol>  
-        {isLoading? (
+        {isLoading ? (
         <p>Loading...</p>
         ) : ( 
           food.analyzedInstructions[0].steps.map((step) => (
